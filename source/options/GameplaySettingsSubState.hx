5018df21c1787f5d497ae05f248a189dd42b6710
ShadowMario <44785097+ShadowMario@users.noreply.github.com>
2023-01-14 15:20:42 -0300

    Forgot to remove the Controller mode option

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -32,13 +32,6 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 		description = 'Options that directly affects your gameplay';
 		rpcTitle = 'Gameplay Settings Menu'; //for Discord Rich Presence
 
-		var option:Option = new Option('Controller Mode',
-			'Check this if you want to play with\na controller instead of using your Keyboard. Can be buggy with some controllers.',
-			'controllerMode',
-			'bool',
-			false);
-		addOption(option);
-
 		//I'd suggest using "Downscroll" as an example for making your own option since it is the simplest here
 		var option:Option = new Option('Downscroll', //Name
 			'If checked, notes go Down instead of Up', //Description
