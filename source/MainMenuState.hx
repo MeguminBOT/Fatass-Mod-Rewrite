6e3305631348b8d929c0ee78330ea5ad0dcb76ab
MeguminBOT <discord.meguminbot@gmail.com>
2023-04-14 02:51:17 +0200

    forgor to actually add it to menu

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -34,6 +34,7 @@ class MainMenuState extends MusicBeatState
 		'story_mode',
 		'freeplay',
 		#if MODS_ALLOWED 'mods', #end
+		'download'
 		#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
 		//#if !switch 'donate', #end
