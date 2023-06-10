605d73d4cf9f921019cd6ebf49e7648f35029ccf
MeguminBOT <discord.meguminbot@gmail.com>
2023-06-10 13:18:43 +0200

    Optimize GPU and audio stuff (Remove duplicates)

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -15,7 +15,6 @@ class ClientPrefs {
 	public static var globalAntialiasing:Bool = true;
 	public static var noteSplashes:Bool = true;
 	public static var lowQuality:Bool = false;
-	public static var cacheOnGPU:Bool = true;
 	public static var shaders:Bool = true;
 	public static var framerate:Int = 240;
 	public static var cursing:Bool = true;
@@ -130,7 +129,6 @@ class ClientPrefs {
 		FlxG.save.data.globalAntialiasing = globalAntialiasing;
 		FlxG.save.data.noteSplashes = noteSplashes;
 		FlxG.save.data.lowQuality = lowQuality;
-		FlxG.save.data.cacheOnGPU = cacheOnGPU;
 		FlxG.save.data.shaders = shaders;
 		FlxG.save.data.framerate = framerate;
 		//FlxG.save.data.cursing = cursing;
@@ -225,9 +223,6 @@ class ClientPrefs {
 		}
 		if(FlxG.save.data.lowQuality != null) {
 			lowQuality = FlxG.save.data.lowQuality;
-		}
-		if(FlxG.save.data.cacheOnGPU != null) {
-			cacheOnGPU = FlxG.save.data.cacheOnGPU;
 		}
 		if(FlxG.save.data.shaders != null) {
 			shaders = FlxG.save.data.shaders;
